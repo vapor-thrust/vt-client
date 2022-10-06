d9136a94181cb474f7307d938569a28788f96d68
teperie <77098015+teperie@users.noreply.github.com>
2022-10-06 12:43:51 +0900

    feat: implements mui
    #4

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 import logo from './logo.svg'
 import './App.css'
 
@@ -10,14 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="contained">새로 만든 버튼이에용</Button>
       </header>
     </div>
   )
