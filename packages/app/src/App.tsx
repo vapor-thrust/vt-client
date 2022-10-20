715059bfdd89e291e05b2654ba83394081346f52
teperie <77098015+teperie@users.noreply.github.com>
2022-10-20 13:11:24 +0900

    test: service attachment

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import TextField from '@mui/material/TextField'
+import Container from '@mui/material/Container'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+
+const theme = createTheme()
 
 function App() {
+  // const ref = useRef()
+  const submit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+    console.log(data.get('a'))
+
+    fetch('0.0.0.0/oper', {
+      method: 'POST',
+      body: JSON.stringify({
+        a: data.get('a'),
+        b: data.get('b'),
+      }),
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <Box component="form" marginTop="20px" onSubmit={submit}>
+          <TextField id="a" name="a" label="a값" required></TextField>
+          <TextField id="b" name="b" label="b값" required></TextField>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            get
+          </Button>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
 
