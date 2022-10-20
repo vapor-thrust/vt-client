a4ab958f29416a233d8fcaa20f083b5cb7159ca8
teperie <77098015+teperie@users.noreply.github.com>
2022-10-20 14:25:53 +0900

    chore: added server address input field

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -10,6 +10,7 @@ const theme = createTheme()
 
 function App() {
   const [result, setResult] = useState('')
+  const [address, setAddress] = useState('http://localhost:8000/oper')
 
   const submit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -17,15 +18,14 @@ function App() {
     const data = new FormData(event.currentTarget)
     const body = JSON.stringify({ a: data.get('a'), b: data.get('b') })
 
-    fetch('http://localhost:8000/oper', {
+    fetch(address, {
       method: 'POST',
       body,
     })
-      .then((response) =>
-        response.json().catch(() => {
-          return { res: 'wtf' }
-        })
-      )
+      .then((response) => response.json())
+      .catch(() => {
+        return { res: 'wtf' }
+      })
       .then((json) => setResult(JSON.stringify(json)))
   }
 
@@ -35,6 +35,16 @@ function App() {
         <Box component="form" marginTop="20px" onSubmit={submit}>
           <TextField id="a" name="a" label="a값" required></TextField>
           <TextField id="b" name="b" label="b값" required></TextField>
+
+          <TextField
+            id="address"
+            name="b"
+            label="서버 주소"
+            required
+            fullWidth
+            value={address}
+            onChange={(event) => setAddress(event.target.value)}
+          ></TextField>
 
           <Button
             type="submit"
